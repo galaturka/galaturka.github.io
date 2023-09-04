58c1c0ce8da97bee48caa64f58a069402f6c5adb
Gorkem <35394185+galaturka@users.noreply.github.com>
2023-09-04 16:32:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Replace 'YOUR_API_KEY' with your actual OpenAI API key
 const apiKey = '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';
-const apiUrl = 'https://api.openai.com/v1/engines/davinci/completions';
+const apiUrl = 'https://api.openai.com/v1/completions';
 
 function fetchOpenAIResponse() {
     const prompt = document.getElementById('prompt').value;
@@ -13,6 +13,7 @@ function fetchOpenAIResponse() {
 
     const requestBody = {
         prompt: prompt,
+        model: "text-davinci-003",
         max_tokens: 50, // Adjust the desired response length
     };
 
